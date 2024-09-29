1c544343b805313634625403d10520430fcda308
thangld <thangle84264@gmail.com>
2024-09-29 22:22:27 +0700

    Applied DB

--- SWP391_Group6/src/java/dal/DBContext.java
+++ SWP391_Group6/src/java/dal/DBContext.java
@@ -14,10 +14,9 @@ public abstract class DBContext<T> {
 
     public DBContext() {
         try {
-            String username = "thangle";
-            String password = "1407";
-            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=demo;encrypt=true;trustservercertificate=true;";
-
+            String username = "son";
+            String password = "Son@123@";
+            String url = "jdbc:sqlserver://103.252.92.181\\SQLEXPRESS:1433;databaseName=swp-son;encrypt=true;trustservercertificate=true;";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connect = DriverManager.getConnection(url, username, password);
         } catch (SQLException ex) {
